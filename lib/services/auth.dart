e03fc4c16e2d2129f9f1c26088f61b597e2b4a79
Antonate-96 <bignut.maoo@gmail.com>
2022-06-24 10:35:02 +0700

    add database firestore chap17

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_app/models/user.dart';
+import 'package:firebase_app/services/database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -48,6 +49,9 @@ class Authservice {
       UserCredential result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       User? user = result.user;
+      // create a new document for new user with uid
+      await DatabaseService(uid: user!.uid)
+          .updateUserData('0', 'new crew member', 100);
       return _userfromfirebaseUser(user);
     } catch (e) {
       print(e.toString());
